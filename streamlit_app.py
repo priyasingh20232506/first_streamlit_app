ee5f979cd8d6dba850ee94750264d84942e82410
priyasingh20232506 <135512369+priyasingh20232506@users.noreply.github.com>
2023-06-04 18:59:16 +0530

    mom's favourites streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
-import streamlit
-streamlit.title("My Parents healthy Diner")
-streamlit.header("Breakfast Menu")
-streamlit.text( "Omega3 & Blueberries oatmeal")
-streamlit.text( "Kale, Spinach & Rocket Smoothie")
-streamlit.text("Hard Boiled Free- Range Egg")
+import streamlit     
+streamlit.title("My Mom's New Healthy Diner")
+streamlit.header("Breakfast Favourites")
+streamlit.text( " 🥣 Omega3 & Blueberries oatmeal")
+streamlit.text( " 🥗 Kale, Spinach & Rocket Smoothie")
+streamlit.text(" 🐔 Hard Boiled Free- Range Egg")
+streamlit.text(" 🥑🍞 Avacado Toast ")
