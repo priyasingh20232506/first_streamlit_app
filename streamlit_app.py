e72975b4702bee34f9c2c7af5dfc557281e1bdb6
priyasingh20232506 <135512369+priyasingh20232506@users.noreply.github.com>
2023-06-04 19:00:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,6 @@ streamlit.text( " 🥣 Omega3 & Blueberries oatmeal")
 streamlit.text( " 🥗 Kale, Spinach & Rocket Smoothie")
 streamlit.text(" 🐔 Hard Boiled Free- Range Egg")
 streamlit.text(" 🥑🍞 Avacado Toast ")
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
